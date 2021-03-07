daaa231e8e986898726c0e49ce03d8df5bc1ee14
nope0124 <kami-ryo0124@ezweb.ne.jp>
2021-03-07 21:14:30 +0900

    update

--- ocr/views.py
+++ ocr/views.py
@@ -8,51 +8,18 @@ import keras
 from keras.models import load_model
 
 def img2frm(img):
+    W = 1000
+    H = 300
+    SP = 3
+    ans = ""
     gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    for i in range(100):
-        for j in range(800):
+    for i in range(H):
+        for j in range(W):
             gray_img[i][j] = 255 - gray_img[i][j]
-
     ret, bin_img = cv2.threshold(gray_img, 128, 255, cv2.THRESH_BINARY)
-
-    bw = [0 for i in range(800)]
-    for i in range(100):
-        for j in range(800):
-            if bin_img[i][j] == 255:
-                bw[j] = 1
-                
-    lr = []
-    s = -1
-    for i in range(800):
-        if bw[i] == 1:
-            if s == -1:
-                s = i
-        elif bw[i] == 0:
-            if s != -1:
-                lr.append([s, i - 1])
-                s = -1
-
-    if s != -1:
-        lr.append([s, 799])
-        
     model = keras.models.load_model('model.h5', compile=False)
-
-    pixels_test = []
-
-    for p in lr:
-        tmp_img = np.zeros((100, p[1] - p[0] + 1 + 30), dtype='uint8')
-        for i in range(100):
-            for j in range(p[0], p[1] + 1):
-                tmp_img[i][j + 15 - p[0]] = bin_img[i][j]
-        pixels_test.append(cv2.resize(tmp_img, (28, 28)))
-    S = np.array(pixels_test) / 255
-
-    S = S.reshape(-1, 28, 28, 1)
-
-    pred = model.predict(S)
-    preds = np.argmax(pred, axis=1)
-
     num2code = {
+        0: "0",
         1: "1",
         2: "2",
         3: "3",
@@ -66,10 +33,53 @@ def img2frm(img):
         11: "-",
     }
 
-    ans = ""
-    for i in preds:
-        ans += num2code[i]
+    for sp in range(SP):
+        
+        bw = [0 for i in range(W)]
+        for i in range(H // SP):
+            for j in range(W):
+                if bin_img[i + sp * H // SP][j] == 255:
+                    bw[j] = 1
+                    
+        lr = []
+        s = -1
+        for i in range(W):
+            if bw[i] == 1:
+                if s == -1:
+                    s = i
+            elif bw[i] == 0:
+                if s != -1:
+                    lr.append([s, i - 1])
+                    s = -1
+        if s != -1:
+            lr.append([s, W - 1])
+            
+        pixels_test = []
 
+        for p in lr:
+            tmp_img = np.zeros((H // SP, p[1] - p[0] + 1 + 30), dtype='uint8')
+            for i in range(H // SP):
+                for j in range(p[0], p[1] + 1):
+                    tmp_img[i][j + 15 - p[0]] = bin_img[i + sp * H // SP][j]
+            pixels_test.append(cv2.resize(tmp_img, (28, 28)))
+        S = np.array(pixels_test) / 255
+        S = S.reshape(-1, 28, 28, 1)
+        if len(S) == 0:
+            continue
+        ans += "<br>"
+        st = ""
+        pred = model.predict(S)
+        preds = np.argmax(pred, axis=1)
+        for i in preds:
+            st += num2code[i]
+        try:
+            sum = eval(st)
+            ans += st + "=" + str(sum)
+        except ZeroDivisionError:
+            ans += st + " " + "(0除算が発生しています)"
+        except SyntaxError:
+            ans += st + " " + "(計算式が間違っています)"
+        
     return ans
 
 
@@ -96,15 +106,7 @@ def index(request):
         keyword = base64.b64decode(keyword) #base64をデコード
         jpg=np.frombuffer(keyword, dtype=np.uint8) #raw image <- jpg
         img = cv2.imdecode(jpg, cv2.IMREAD_COLOR) #画像を保存する場合
-        st = img2frm(img)
-        ans = ""
-        try:
-            sum = eval(st)
-            ans = st + "=" + str(sum)
-        except ZeroDivisionError:
-            ans = st + " " + "(0除算が発生しています)"
-        except SyntaxError:
-            ans = st + " " + "(計算式が間違っています)"
+        ans = img2frm(img)
         
         data = {"status":ans}
         
