228e35a661c2f0eef47ba0a24a63c0d6ff492848
nope0124 <kami-ryo0124@ezweb.ne.jp>
2021-03-09 20:27:05 +0900

    st

--- ocr/views.py
+++ ocr/views.py
@@ -100,14 +100,15 @@ def img2frm(img):
             elif i == 13:
                 st += " " + num2code[i] + " "
                 dis += " ÷ "
-        try:
-            sum = eval(st)
-            ans += dis + " = " + str(sum)
-        except ZeroDivisionError:
-            ans += dis + " " + "(0除算が発生しています)"
-        except SyntaxError:
-            ans += dis + " " + "(計算式が間違っています)"
-        ans = "ok"
+        ans += st
+#        try:
+#            sum = eval(st)
+#            ans += dis + " = " + str(sum)
+#        except ZeroDivisionError:
+#            ans += dis + " " + "(0除算が発生しています)"
+#        except SyntaxError:
+#            ans += dis + " " + "(計算式が間違っています)"
+        
     return ans
 
 
